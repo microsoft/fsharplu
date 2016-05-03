3a7ed90bf2448ed924450b643caeba9162e826ab
William Blum <wiblum@microsoft.com>
2016-05-03 17:39:42 +0000

    PR 707: Json serialization handling of nested Some types with unit tests
     - Json serialization handling of nested Some types with unit tests

--- FSharpLu.Json/DiscriminatedUnions.fs
+++ FSharpLu.Json/DiscriminatedUnions.fs
@@ -11,6 +11,9 @@ open Microsoft.FSharp.Reflection
 /// Improved Json converter for discriminated unions and option types ('a option). 
 type DiscriminatedUnionJsonConverter() =
     inherit Newtonsoft.Json.JsonConverter()
+
+    let isOptionType (t:System.Type) = 
+        t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
         
     override __.CanConvert(objectType:System.Type) = 
         // Include F# discriminated unions
@@ -21,12 +24,40 @@ type DiscriminatedUnionJsonConverter() =
     override __.WriteJson(writer:JsonWriter, value:obj, serializer:JsonSerializer) =
         let t = value.GetType()
         // Option type?
-        if t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>> then
-            if isNull value then
-                ()
-            else 
-                let _,fields = FSharpValue.GetUnionFields(value, t)
-                serializer.Serialize(writer, fields.[0])
+        if isOptionType t then
+            let cases = FSharpType.GetUnionCases(t)
+            let none, some = cases.[0], cases.[1]
+    
+            let case, fields = FSharpValue.GetUnionFields(value, t)
+            
+            if case = none then
+                () // None is serialized as just null
+
+            // Some _
+            else
+                // Handle cases `Some None` and `Some null`
+                let innerType = some.GetFields().[0].PropertyType
+                let innerValue = fields.[0]
+                if isNull innerValue then
+                    writer.WriteStartObject()
+                    writer.WritePropertyName("Some")
+                    writer.WriteNull()
+                    writer.WriteEndObject()
+                // Some v with v <> null && v <> None
+                else
+                    // Is it a nested option: `(e.g., "Some (Some ... Some ( ... )))"`?
+                    if isOptionType <| innerType then
+                        // Preserved the nested structure through boxing
+                        writer.WriteStartObject()
+                        writer.WritePropertyName("Some")
+                        serializer.Serialize(writer, innerValue)
+                        writer.WriteEndObject()
+                    else
+                        // Type is option<'a> where 'a is NOT an option type itself
+                        // => we can simplify the Json by omitting the `Some` boxing
+                        // and serializing the nested object directly
+                        serializer.Serialize(writer, innerValue)
+
         // Discriminated union
         else
             let case, fields = FSharpValue.GetUnionFields(value, t)
@@ -50,15 +81,45 @@ type DiscriminatedUnionJsonConverter() =
     override __.ReadJson(reader:JsonReader, objectType:System.Type, existingValue:obj, serializer:JsonSerializer) =
         let cases = FSharpType.GetUnionCases(objectType)
         // Option type?
-        if objectType.IsGenericType && objectType.GetGenericTypeDefinition() = typedefof<option<_>> then
-                let caseNone, caseSome = cases.[0], cases.[1]
-                let jToken = Linq.JToken.ReadFrom(reader)
-                if jToken.Type = Linq.JTokenType.Null then
-                    FSharpValue.MakeUnion(caseNone, [||])
-                else 
-                    let nestedType = objectType.GetGenericArguments().[0] 
-                    let nestedValue = jToken.ToObject(nestedType, serializer)
-                    FSharpValue.MakeUnion(caseSome, [| nestedValue |])
+        if isOptionType objectType then
+            let caseNone, caseSome = cases.[0], cases.[1]
+            let jToken = Linq.JToken.ReadFrom(reader)
+            // Json Null maps to `None`
+            if jToken.Type = Linq.JTokenType.Null then
+                FSharpValue.MakeUnion(caseNone, [||])
+            
+            // Json that is not null maps to `Some _`
+            else
+                let nestedType = objectType.GetGenericArguments().[0]
+                let parseBox (jToken:Linq.JToken) =
+                    if jToken.Type <> Linq.JTokenType.Object then
+                        failwith "Nested option types must be serialized as boxed Json objects"
+
+                    let jObject = jToken :?> Linq.JObject
+                    match jObject.TryGetValue("Some") with
+                    | false, _ ->
+                        failwith "Nested option types must be serialized as boxed Json objects with attribute 'Some'."
+                    | true, unboxedValue ->
+                        if unboxedValue.Type = Linq.JTokenType.Null then
+                            // Case of Json { "Some" : null } for type option<'a> where 'a is nullable
+                            // => deserialized to `Some null`
+                            null
+                        else
+                            // Case of Json { "Some" : <obj> } where <obj> is not null
+                            // => we just deserialize the nested object recursively
+                            unboxedValue.ToObject(nestedType, serializer)
+
+                let nestedValue = 
+                    // Is the nested type an option type itself?
+                    // or is the Json to be deserialized an object Json?
+                    if isOptionType nestedType || jToken.Type = Linq.JTokenType.Object then 
+                        // Nested option type are always boxed in Json to prevent any ambguity
+                        parseBox jToken
+                    else
+                        // type is option<'a> where 'a is not an option type
+                        // => we can deserialize the object of type 'a directly without unboxing
+                        jToken.ToObject(nestedType, serializer)
+                FSharpValue.MakeUnion(caseSome, [| nestedValue |])
                  
         // Discriminated union
         else
--- FSharpLu.Json/Json.fs
+++ FSharpLu.Json/Json.fs
@@ -6,39 +6,39 @@ open Newtonsoft.Json
 type With< ^S when ^S : (static member settings : JsonSerializerSettings) > =
 
     /// Serialize an object to Json with the specified converter
-    static member inline public serialize (obj:'T) =
+    static member inline public serialize (obj:^T) =
         let settings = (^S:(static member settings : JsonSerializerSettings)())
         JsonConvert.SerializeObject(obj, Formatting.Indented, settings)
 
     /// Serialize an object to Json with the specified converter and save the result to a file
-    static member inline public serializeToFile file (obj:'T) =
+    static member inline public serializeToFile file (obj:^T) =
         let settings = (^S:(static member settings : JsonSerializerSettings)())
         let json = JsonConvert.SerializeObject(obj, Formatting.Indented, settings)
         System.IO.File.WriteAllText(file, json)
 
     /// Deserialize a Json to an object of type 'T
-    static member inline public deserialize<'T> json  =
+    static member inline public deserialize< ^T> json  =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
-        JsonConvert.DeserializeObject<'T>(json, settings)
+        JsonConvert.DeserializeObject< ^T>(json, settings)
 
     /// Deserialize a stream to an object of type 'T
-    static member inline public deserializeStream<'T> (stream:System.IO.Stream) =
+    static member inline public deserializeStream< ^T> (stream:System.IO.Stream) =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         let serializer = JsonSerializer.Create(settings)
         use streamReader = new System.IO.StreamReader(stream)
         use jsonTextReader = new JsonTextReader(streamReader)
-        serializer.Deserialize<'T>(jsonTextReader)
+        serializer.Deserialize< ^T>(jsonTextReader)
 
-    /// Read Json from a file and desrialized it to an object of type 'T
-    static member inline deserializeFile<'T> file =
+    /// Read Json from a file and desrialized it to an object of type ^T
+    static member inline deserializeFile< ^T> file =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         System.IO.File.ReadAllText file |> With< ^S>.deserialize
 
-    /// Try to deserialize a stream to an object of type 'T
-    static member inline tryDeserializeStream<'T> stream =
+    /// Try to deserialize a stream to an object of type ^T
+    static member inline tryDeserializeStream< ^T> stream =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         try
-            let o = With< ^S>.deserializeStream<'T> stream
+            let o = With< ^S>.deserializeStream< ^T> stream
             if obj.ReferenceEquals(o, null) then
                 Choice2Of2 <| "Deserialization returned null"
             else
@@ -50,11 +50,11 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings) > =
         | exn ->
             Choice2Of2 <| sprintf "Exception while deserializing stream: %O" exn
 
-    /// Try to deserialize json to an object of type 'T
-    static member inline tryDeserialize<'T> json =
+    /// Try to deserialize json to an object of type ^T
+    static member inline tryDeserialize< ^T> json =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         try
-            let o = With< ^S>.deserialize<'T> json
+            let o = With< ^S>.deserialize< ^T> json
             if obj.ReferenceEquals(o, null) then
                 Choice2Of2 <| "Deserialization returned null"
             else
@@ -67,10 +67,10 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings) > =
             Choice2Of2 <| sprintf "Exception while deserializing string: %O" exn
 
     /// Try to read Json from a file and desrialized it to an object of type 'T
-    static member inline tryDeserializeFile<'T> file =
+    static member inline tryDeserializeFile< ^T> file =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         try
-            let o = With< ^S>.deserializeFile<'T> file
+            let o = With< ^S>.deserializeFile< ^T> file
             if obj.ReferenceEquals(o, null) then
                 Choice2Of2 <| sprintf "Deserialization of %s returned null" file
             else
