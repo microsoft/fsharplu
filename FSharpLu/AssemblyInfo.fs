f1a805c7cc85e96c348d6bfd8f1a4795f1cf74e7
William Blum <wiblum@microsoft.com>
2017-03-14 14:27:43 -0700

    Delay-sign assemblies with Microsoft public strong name key

--- FSharpLu/AssemblyInfo.fs
+++ FSharpLu/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-(* General Information about an assembly is controlled through the following 
+(* General Information about an assembly is controlled through the following
 set of attributes. Change these attribute values to modify the information
 associated with an assembly. *)
 [<assembly: AssemblyTitle("FSharpLu F# Utilities")>]
@@ -16,8 +16,8 @@ associated with an assembly. *)
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-(* Setting ComVisible to false makes the types in this assembly not visible 
-to COM components.  If you need to access a type in this assembly from 
+(* Setting ComVisible to false makes the types in this assembly not visible
+to COM components.  If you need to access a type in this assembly from
 COM, set the ComVisible attribute to true on that type. *)
 [<assembly: ComVisible(false)>]
 
@@ -27,13 +27,16 @@ COM, set the ComVisible attribute to true on that type. *)
 (* Version information for an assembly consists of the following four values:
 
       Major Version
-      Minor Version 
+      Minor Version
       Build Number
       Revision
 
-You can specify all the values or you can default the Build and Revision Numbers 
+You can specify all the values or you can default the Build and Revision Numbers
 by using the '*' as shown below:
 [<assembly: AssemblyVersion("1.0.*")>] *)
 [<assembly: AssemblyVersion("0.9.*")>]
 [<assembly: AssemblyFileVersion("0.9.*")>]
+[<assembly:AssemblyDelaySignAttribute(true)>]
+[<assembly:AssemblyKeyFileAttribute(@"..\msft-StrongName.pub")>]
+
 ()
