12c80ababd95dea66f2f7140b5136d017db7e690
William Blum <wiblum@microsoft.com>
2017-03-19 10:12:51 -0700

    Fix leak in startProcessAsync #20

--- FSharpLu/Diagnostics.fs
+++ FSharpLu/Diagnostics.fs
@@ -11,7 +11,7 @@ let isElevated () =
     principal.IsInRole(WindowsBuiltInRole.Administrator)
 
 /// ensure that the process runs with elevated permissions
-let ensureAdmin () = 
+let ensureAdmin () =
     if not (isElevated ()) then
         invalidOp "Elevated privileges required."
 
@@ -51,101 +51,96 @@ module Process =
     /// Starts a process and returns an asynchronous workflow that waits
     /// for it to terminate
     let startProcessAsync command arguments workingDir (flags:ProcessStartFlags) (timeout:ProcessTimeoutAction) =
-        
-        let elevated, minimized, redirectStdOut =
-            flags.HasFlag ProcessStartFlags.Elevated,
-            flags.HasFlag ProcessStartFlags.Minimized,
-            flags.HasFlag ProcessStartFlags.RedirectStandardOutput
-        
-        let useShellExecute =
-            if minimized then
-                Trace.warning "Important: Shell execute may pop-up the IE security zone window which synchronously blocks the call to Start()! To run minimized shell execute has to be used"
-                true
+        async {
+            let useShellExecute =
+                if flags.HasFlag ProcessStartFlags.Minimized then
+                    Trace.warning "Important: Shell execute may pop-up the IE security zone window which synchronously blocks the call to Start()! To run minimized shell execute has to be used"
+                    true
+                else
+                    false
+
+            use instance =
+                new Process(
+                    StartInfo =
+                        ProcessStartInfo
+                            (
+                                FileName = command,
+                                WorkingDirectory = workingDir,
+                                Arguments = arguments,
+                                CreateNoWindow = false,
+                                UseShellExecute = useShellExecute,
+                                RedirectStandardOutput = flags.HasFlag ProcessStartFlags.RedirectStandardOutput,
+                                WindowStyle = (
+                                    if flags.HasFlag ProcessStartFlags.Minimized then
+                                        ProcessWindowStyle.Minimized
+                                    else
+                                        ProcessWindowStyle.Normal
+                                    ),
+
+                                Verb = (
+                                    if flags.HasFlag ProcessStartFlags.Elevated then
+                                         "runas"
+                                    else
+                                        String.Empty)
+                            ),
+                    EnableRaisingEvents = true)
+
+            Trace.info "Launching '%s %s'" command arguments
+            let timer = System.Diagnostics.Stopwatch()
+            timer.Start()
+            instance.Exited.Add
+                (fun a -> timer.Stop()
+                          Trace.info "Process execution terminated in %O with error code 0x%X: '%O %O'" timer.Elapsed (int32 instance.ExitCode) command arguments)
+
+            if not (instance.Start()) then
+                let message = sprintf "Could not start command: '%s' with parameters '%s'" command arguments
+                return raise <| System.InvalidOperationException(message)
             else
-                false
-        
-        let instance =
-            new Process(
-                StartInfo =
-                    ProcessStartInfo
-                        (
-                            FileName = command,
-                            WorkingDirectory = workingDir,
-                            Arguments = arguments,
-                            CreateNoWindow = false,
-                            UseShellExecute = useShellExecute,
-                            RedirectStandardOutput = redirectStdOut,
-                            WindowStyle = (
-                                if minimized then
-                                    ProcessWindowStyle.Minimized
-                                else
-                                    ProcessWindowStyle.Normal
-                                ),
+                let processExitedResult() =
+                    {
+                        ProcessResult.ProcessExited = true
+                        ExitCode = instance.ExitCode
+                        ExecutionTime = timer.Elapsed
+                        StandardOutput =
+                            if flags.HasFlag ProcessStartFlags.RedirectStandardOutput then
+                                instance.StandardOutput.ReadToEnd()
+                            else
+                                String.Empty
+                    }
 
-                            Verb = (
-                                if elevated then 
-                                     "runas"
-                                else
-                                    String.Empty)
-                        ),
-                EnableRaisingEvents = true)
+                let runProcessWithTimeout (t:TimeSpan) (kill: bool) =
+                    async {
+                        try
+                            let! tsk = Async.StartChild(Async.AwaitEvent(instance.Exited), int t.TotalMilliseconds)
+                            let! _ = tsk
+                            Trace.info "%s %s exited with code: %d" command arguments instance.ExitCode
+                            return processExitedResult()
+                        with
+                        | :? System.TimeoutException ->
+                            Trace.info "%s %s did not exit within allocated time out of %f seconds. Should process be killed: %A" command arguments t.TotalSeconds kill
 
-        Trace.info "Launching '%s %s'" command arguments
-        let startTime = System.DateTime.UtcNow
-        instance.Exited.Add (fun a -> let endTime = System.DateTime.UtcNow
-                                      if instance.ExitCode = 0 then
-                                          Trace.info "Process executed successfully in %O: '%O %O'" (endTime-startTime) command arguments
-                                      else
-                                          Trace.info "Process execution failed in %O with error code 0x%X: '%O %O'" (endTime-startTime) (int32 instance.ExitCode) command arguments)
+                            if kill then
+                                try instance.Kill() with _ -> ()
 
-        if not (instance.Start()) then
-            let message = sprintf "Could not start command: '%s' with parameters '%s'" command arguments
-            raise <| System.InvalidOperationException(message)
-        else
-            let processExitedResult() =
-                {
-                    ProcessResult.ProcessExited = true
-                    ExitCode = instance.ExitCode
-                    ExecutionTime = System.DateTime.UtcNow - startTime
-                    StandardOutput =
-                        if redirectStdOut then 
-                            instance.StandardOutput.ReadToEnd() 
-                        else 
-                            String.Empty
-                }
+                            return
+                                {
+                                    ProcessResult.ProcessExited = false
+                                    ExitCode = -1
+                                    ExecutionTime = timer.Elapsed
+                                    StandardOutput = String.Empty
+                                }
+                    }
 
-            let runProcessWithTimeout (t:TimeSpan) (kill: bool) =
-                async {
-                    try
-                        let! tsk = Async.StartChild(Async.AwaitEvent(instance.Exited), int t.TotalMilliseconds)
-                        let! _ = tsk
-                        Trace.info "%s %s exited with code: %d" command arguments instance.ExitCode
-                        return processExitedResult()
-                    with
-                    | :? System.TimeoutException ->
-                        Trace.info "%s %s did not exit within allocated time out of %f seconds. Should process be killed: %A" command arguments t.TotalSeconds kill
-                    
-                        if kill then
-                            try instance.Kill() with _ -> ()
-
-                        return
-                            {
-                                ProcessResult.ProcessExited = false
-                                ExitCode = -1
-                                ExecutionTime = System.DateTime.UtcNow - startTime
-                                StandardOutput = String.Empty
-                            }
-                }
-
-            async {
                 match timeout with
                 | NoTimeout ->
                     let! _ = Async.AwaitEvent(instance.Exited)
-                    Trace.info "%s %s exited with code: %d" command arguments instance.ExitCode
+                    Trace.verbose "%s %s exited with code: %d" command arguments instance.ExitCode
                     return processExitedResult()
-                | AttemptToKillProcessAfterTimeout t -> return! runProcessWithTimeout t true
-                | KeepTheProcessRunningAfterTimeout t -> return! runProcessWithTimeout t false
-            }
+                | AttemptToKillProcessAfterTimeout t ->
+                    return! runProcessWithTimeout t true
+                | KeepTheProcessRunningAfterTimeout t ->
+                    return! runProcessWithTimeout t false
+        }
 
     // Start a process and returns an asynchronous workflow that waits
     // for it to terminate and return the process exit code
