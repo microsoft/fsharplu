8e69449a5ee3ed41d412b31b0d02d30452aeea7e
William Blum <wiblum@microsoft.com>
2017-03-20 23:41:57 -0700

    statically linking json.net to avoid nuget dependency hell issues

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,17 +4,25 @@
 #   Rebuild the solution instead of just building
 #.PARAMETER delaysign
 #   Delay-sign the assemblies after running the build
+#.PARAMETER embedJsonNet
+#   Embed NewtonSoft JSon.Net DLL in the target assembly. This option can be used to avoid diamond dependency hell issues when using NewtonSoft JSon.Net in a larger project.
 param(
     [switch]$delaysign,
-    [switch]$rebuild
+    [switch]$rebuild,
+    [switch]$embedJsonNet
 )
 
 $ErrorActionPreference = 'stop'
 $root = Split-Path -parent $psScriptRoot
 $msbuild = "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\amd64\msbuild.exe"
+$options = @()
+
+if($embedJsonNet) {
+    $options += '/p:EmbedJsonNet=true'
+}
 
 if($delaysign) {
-    $options = "/p:CompilationSymbols=DELAYSIGNING"
+    $options += '/p:CompilationSymbols="DELAYSIGNING" '
     if(-not $rebuild) {
         $rebuild = $true
         Write-Warning "Delaying signing requested: forcing rebuild"
@@ -27,6 +35,7 @@ if($rebuild) {
     $target = 'Build'
 }
 
+write-host  $msbuild $root\FSharpLu.sln /t:$target /p:Configuration=Release /p:Platform="Any CPU" $options /p:EmbedJsonNet=true
 & $msbuild $root\FSharpLu.sln /t:$target /p:Configuration=Release /p:Platform="Any CPU" $options
 
 if($delaysign) {
