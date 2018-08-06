92c89b615e1191e92487dc345f5934d26fb00487
Christian Steinert <chr.steinert@outlook.com>
2018-08-06 09:39:23 +0200

    Removes static type parameters from …ToType function.

--- FSharpLu.Json/WithFunctor.fs
+++ FSharpLu.Json/WithFunctor.fs
@@ -32,7 +32,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         JsonConvert.DeserializeObject< ^T>(json, settings)
     
     /// Deserialize a Json to an object of the targetType's type
-    static member inline public deserializeToType< ^T> targetType json : ^T =
+    static member inline public deserializeToType targetType json : ^T =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         JsonConvert.DeserializeObject(json, targetType, settings) :?> ^T
 
@@ -45,7 +45,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         serializer.Deserialize< ^T>(jsonTextReader)
 
     /// Deserialize a stream to an object of type of targetType
-    static member inline public deserializeStreamToType< ^T> targetType (stream:System.IO.Stream) =
+    static member inline public deserializeStreamToType targetType (stream:System.IO.Stream) =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         let serializer = JsonSerializer.Create(settings)
         use streamReader = new System.IO.StreamReader(stream)
@@ -58,7 +58,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         System.IO.File.ReadAllText file |> With< ^S>.deserialize
         
     /// Read Json from a file and desrialized it to an object of targetType
-    static member inline deserializeFileToType< ^T> targetType file :^T =
+    static member inline deserializeFileToType targetType file :^T =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         System.IO.File.ReadAllText file |> With< ^S>.deserializeToType targetType
 
@@ -69,7 +69,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         |> Helpers.exceptionToString
 
     /// Try to deserialize a stream to an object of targetType
-    static member inline tryDeserializeStreamToType< ^T> stream targetType =
+    static member inline tryDeserializeStreamToType stream targetType =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         Helpers.tryCatchJsonSerializationException< ^T, System.IO.Stream> false (With< ^S>.deserializeStream targetType) stream
         |> Helpers.exceptionToString
@@ -81,7 +81,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         |> Helpers.exceptionToString
 
     /// Try to deserialize json to an object of targetType
-    static member inline tryDeserializeToType< ^T> targetType json =
+    static member inline tryDeserializeToType targetType json =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         Helpers.tryCatchJsonSerializationException< ^T, string> false (With< ^S>.deserialize targetType) json
         |> Helpers.exceptionToString
@@ -93,7 +93,7 @@ type With< ^S when ^S : (static member settings : JsonSerializerSettings)
         |> Helpers.exceptionToString
         
     /// Try to read Json from a file and desrialized it to an object of targetType
-    static member inline tryDeserializeFileToType< ^T> targetType file =
+    static member inline tryDeserializeFileToType targetType file =
         let settings = (^S:(static member settings :  JsonSerializerSettings)())
         Helpers.tryCatchJsonSerializationException< ^T, string> false (With< ^S>.deserializeFile targetType) file
         |> Helpers.exceptionToString
